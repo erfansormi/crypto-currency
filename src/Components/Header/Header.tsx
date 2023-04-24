d3fbe52d0f7c5ab589c4bad2c43b221d62c0b0bd
Erfan <erfansormi97@gmail.com>
2023-04-24 13:24:12 +0330

    added sticky position to header

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -6,7 +6,11 @@ import { Box, Divider } from '@mui/material'
 
 const Header = () => {
     return (
-        <Box className="border-b" component={"header"} sx={{ bgcolor: "background.default", color: "text.primary", fontWeight: "bold" }}>
+        <Box
+            className="border-b sticky -top-18 right-0 left-0"
+            component={"header"}
+            sx={{ bgcolor: "background.default", color: "text.primary", fontWeight: "bold", zIndex: 50 }}
+        >
             <HeaderLayout>
                 <TopNav />
             </HeaderLayout>
